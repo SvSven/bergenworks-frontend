15798aab77d921fc5bbf10d5405e90befe491045
Sven van Steenis <sven.van.steenis@no.experis.com>
2019-09-26 20:28:50 +0200

    wip: get to work on retrieving page data from wp api

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Layout from "../layouts/main";
+import Axios from "axios";
 
-const Home = () => (
-  <Layout>
-    <h1>home</h1>
-  </Layout>
-);
+const Home = props => {
+  return (
+    <Layout>
+      <h1>{props.title.rendered}</h1>
+      <div dangerouslySetInnerHTML={{ __html: props.content.rendered }}></div>
+    </Layout>
+  );
+};
+
+Home.getInitialProps = async function() {
+  const response = await Axios.get(`${process.env.API_HOST}/bw/frontpage`, {
+    proxy: {
+      host: "localhost",
+      port: 80
+    }
+  });
+
+  return response.data;
+};
 
 export default Home;

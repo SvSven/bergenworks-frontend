5edbf804f9fa99ca8ad5ba35dce07533f9d9b2ca
Sven van Steenis <sven.van.steenis@no.experis.com>
2019-09-27 14:56:50 +0200

    render main menu component on main layout

--- layouts/main.js
+++ layouts/main.js
@@ -1,10 +1,12 @@
 import Meta from "../components/html-head/meta";
+import MainMenu from "../components/navigation/MainMenu";
 
 const MainLayout = props => {
   return (
     <>
       <Meta />
-      <div>{props.children}</div>
+      <MainMenu items={props.menus["main-menu"]} />
+      <>{props.children}</>
     </>
   );
 };

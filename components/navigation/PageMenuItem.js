9c97d488f4a3fce151500e3beb002544e5389820
Sven van Steenis <sven.van.steenis@no.experis.com>
2019-09-29 18:04:46 +0200

    set home menu item to home instead of page url

--- components/navigation/PageMenuItem.js
+++ components/navigation/PageMenuItem.js
@@ -1,9 +1,23 @@
 import Link from "next/link";
 
 const PageMenuItem = props => {
+  const slug = props.is_frontpage ? "/" : props.slug;
+
+  let linkAttributes = {
+    href: "",
+    as: null
+  };
+
+  if (props.is_frontpage) {
+    linkAttributes.href = "/";
+  } else {
+    linkAttributes.href = "/[page]";
+    linkAttributes.as = `/${slug}`;
+  }
+
   return (
     <li>
-      <Link href="/[page]" as={`/${props.slug}`}>
+      <Link {...linkAttributes}>
         <a>{props.title}</a>
       </Link>
     </li>

1973b31b0ca6adc383eabded11101ca22ce9ee6a
Sven van Steenis <sven.van.steenis@no.experis.com>
2019-09-30 12:25:23 +0200

    add styling for main menu

--- components/navigation/MainMenu.js
+++ components/navigation/MainMenu.js
@@ -1,26 +1,37 @@
 import PageMenuItem from "./PageMenuItem";
 import PostMenuItem from "./PostMenuItem";
 import CustomMenuItem from "./CustomMenuItem";
+import Link from "next/link";
+
+import "./main-menu.scss";
 
 const MainMenu = props => {
   return (
-    <nav>
-      {props.items ? (
-        <ul>
-          {props.items.map(item => {
-            // TODO: check if this works with custom post_types...
-            switch (item.type) {
-              case "page":
-                return <PageMenuItem key={item.id} {...item} />;
-              case "post":
-                return <PostMenuItem key={item.id} {...item} />;
-              case "custom":
-                return <CustomMenuItem key={item.id} {...item} />;
-            }
-          })}
-        </ul>
-      ) : null}
-    </nav>
+    <div className="navbar">
+      <nav className="navbar-inner container">
+        <div className="navbar-brand">
+          <Link href="/">
+            <a>BW</a>
+          </Link>
+        </div>
+
+        {props.items ? (
+          <ul className="navbar-menu">
+            {props.items.map(item => {
+              // TODO: check if this works with custom post_types...
+              switch (item.type) {
+                case "page":
+                  return <PageMenuItem key={item.id} {...item} />;
+                case "post":
+                  return <PostMenuItem key={item.id} {...item} />;
+                case "custom":
+                  return <CustomMenuItem key={item.id} {...item} />;
+              }
+            })}
+          </ul>
+        ) : null}
+      </nav>
+    </div>
   );
 };
 

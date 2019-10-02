d8b80ef9a18ef1dc5b6d5e326594a66c3889cc35
Sven van Steenis <sven.van.steenis@no.experis.com>
2019-10-02 21:46:38 +0200

    add component to render a contact list

---
+++ components/shared/ContactList.js
@@ -0,0 +1,29 @@
+import { useSelector } from "react-redux";
+
+const ContactList = props => {
+  const email = useSelector(state => state.contact_email);
+  const phone = useSelector(state => state.contact_phone);
+  const address = useSelector(state => state.contact_address);
+
+  const address_string = `${address.street}, ${address.postal}, ${address.city}`;
+
+  return (
+    <ul className={props.className || null}>
+      {email ? (
+        <li>
+          <a href={`mailto:${email}`}>{email}</a>
+        </li>
+      ) : null}
+
+      {phone ? (
+        <li>
+          <a href={`tel:${phone.replace(/\s/g, "")}`}>{phone}</a>
+        </li>
+      ) : null}
+
+      {address_string ? <li>{address_string}</li> : null}
+    </ul>
+  );
+};
+
+export default ContactList;

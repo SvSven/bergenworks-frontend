ccf6059c6e953d6a7dfc771c0d1d66688395c595
Sven van Steenis <sven.vsteenis@gmail.com>
2019-10-09 20:41:34 +0200

    add layout for featured image and styling on general page

--- pages/post/[post].js
+++ pages/post/[post].js
@@ -2,14 +2,29 @@ import API from "../../utils/api";
 
 const Post = props => {
   return (
-    <div>
-      <h1>{props.title ? props.title.rendered : null}</h1>
+    <>
+      {props.featured_image ? (
+        <div
+          className="page-hero"
+          style={{ backgroundImage: `url(${props.featured_image})` }}
+        ></div>
+      ) : null}
+
       <div
-        dangerouslySetInnerHTML={{
-          __html: props.content ? props.content.rendered : null
-        }}
-      ></div>
-    </div>
+        className={`page-content container ${
+          props.featured_image ? "has-hero" : null
+        }`}
+      >
+        <h1 className="page-title">
+          {props.title ? props.title.rendered : null}
+        </h1>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: props.content ? props.content.rendered : null
+          }}
+        ></div>
+      </div>
+    </>
   );
 };
 

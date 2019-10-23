a05399029f000a0bc5e590daf7ca5db85ed4d141
Sven van Steenis <sven.vsteenis@gmail.com>
2019-10-23 21:05:17 +0200

    add hero section to home page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,27 @@
 import React from "react";
 import API from "../utils/api";
+import Hero from "../components/hero-section/Hero";
 
 const Home = props => {
+  const content = props.acf;
+  const hero = content.hero_section;
+
   return (
-    <div>
+    <>
+      <Hero
+        className="home-hero"
+        title={hero.content.title}
+        subtitle={hero.content.subtitle}
+        image={hero.content.image.sizes.page_hero}
+      />
+
       <h1>{props.title ? props.title.rendered : null}</h1>
       <div
         dangerouslySetInnerHTML={{
           __html: props.content ? props.content.rendered : null
         }}
       ></div>
-    </div>
+    </>
   );
 };
 
